feee0a51b89f6f4479576690b1281b8a1d5cc25c
KojiDB <erotic.nightmares@hotmail.co.jp>
2021-12-02 17:37:51 +0900

    ActiveStorage導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,5 +3,8 @@ class Item < ApplicationRecord
     validates :name, length: { maximum: 40 }
     validatas :introduction, length: { maximum: 1000 }
     validates :price, numericality: {in: 300..9999999}
+    validates :image
   end
+  belongs_to :user
+  has_one_attached :image
 end
